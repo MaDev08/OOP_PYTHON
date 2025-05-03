e4796c28ca392831474c1f850f5aaf2f9837e812
Mateus Aureliano <mateusudo32@gmail.com>
2025-05-02 23:15:06 -0300

    adicionado a herança em python

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 #É uma boa convenção o nome de uma classe sempre começar com uma letra maiúscula.
 class Pessoa(): 
+    total_pessoas = 0 #variável de classe
     def __init__(self, nome, idade): #A função __init__ serve para iniciar toda classe que for criada.
         self.nome = nome #o self serve para especificar que o "nome" se refere a esta classe e o self é tipo um $this no PHP
         self.idade = idade 
@@ -8,10 +9,18 @@ class Pessoa():
     def nome_vendedor(self): #Isto é um metódo.
         print(f"Meu nome é {self.nome}")
 
+    def mostrar_resultado(self): # isto aqui é um metódo de instância, que utilizei para não ficar fazendo milhares de print
+        return print(f"O nome da pessoa é {self.nome} e ela tem {self.idade}")
 
-pessoa1 = Pessoa("Michael", 45) #Realizei uma instância, onde criei o objeto pessoa1 que pega as características da classe Pessoa.
 
-print(pessoa1.nome)
-pessoa1.nome_vendedor() #Nome do objeto + metódo que vai ser chamado é a forma como eu chamo um metódo
 
-print(f"o nome da pessoa é {pessoa1.nome}")
+#Herança em Python:
+class Mateus(Pessoa):
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
+
+    def __str__(self):
+        return f"Nome: {self.nome} idade: {self.idade}"
+
+mateus = Mateus("Mateus", 45)
+print(mateus)
