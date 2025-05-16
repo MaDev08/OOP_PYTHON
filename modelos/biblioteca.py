baa8d1fa376ca28ca464f236337c03d7d8eaadbd
Mateus Aureliano <mateusudo32@gmail.com>
2025-05-16 01:06:48 -0300

    atualização e novo código

---
+++ modelos/biblioteca.py
@@ -0,0 +1,34 @@
+class Biblioteca:
+    bibliotecas = []  # Lista para armazenar todas as bibliotecas
+    def __init__(self, nome):
+        self.nome = nome
+        self._ativo = False  # Encapsulamento usando "_" que é o private
+        Biblioteca.bibliotecas.append(self)
+
+    def __str__(self):
+        return self.nome
+    
+    @classmethod
+    def listar_bibliotecas(cls):
+        print(f"{'Nome da Biblioteca'.ljust(25)} | {"Status"}")
+        for biblioteca in Biblioteca.bibliotecas:
+            print(f"{biblioteca.nome.ljust(25)} | {biblioteca.ativo}")
+
+    def alterna_estado(self):
+        self._ativo = not self._ativo  # Alterna entre ativado e desativado
+
+    @property
+    def ativo(self):
+        return "ativada" if self._ativo else "desativada"
+
+
+# Criando instâncias
+biblioteca_cidade = Biblioteca("Biblioteca da cidade")
+biblioteca_shopping = Biblioteca("Biblioteca do shopping")
+
+# Alternando estados
+biblioteca_cidade.alterna_estado()
+biblioteca_cidade.alterna_estado()
+
+# Listando todas as bibliotecas
+Biblioteca.listar_bibliotecas()
